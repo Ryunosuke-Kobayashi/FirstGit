212c499b10c47aa6c511162d137000ece2630f08
Ryunosuke <b1020197@fun.ac.jp>
2022-10-12 13:36:14 +0900

    First

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstGit
+//
+//  Created by 小林龍ノ介 on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstGitApp.swift
+//  FirstGit
+//
+//  Created by 小林龍ノ介 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct FirstGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
